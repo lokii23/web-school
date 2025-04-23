2465a559befef63f1741768e81d3537397cb628f
Jinewick <jwickhernal@gmail.com>
2025-04-23 23:54:37 +0800

    First Commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -82,12 +82,12 @@
       <main class="main">
     
         <!-- Hero Section -->
-        <section id="hero" class="hero section dark-background">
+        <section id="hero" class="hero section ">
     
           <div id="hero-carousel" data-bs-interval="5000" class="container carousel carousel-fade" data-bs-ride="carousel">
     
             <!-- Slide 1 -->
-            <div class="carousel-item active">
+            <div class="carousel-item active slide-bg-1">
               <div class="carousel-container">
                 <h2 class="animate__animated animate__fadeInDown">Profesional Academy of the Philippines
 
@@ -100,7 +100,7 @@
             </div>
     
             <!-- Slide 2 -->
-            <div class="carousel-item">
+            <div class="carousel-item slide-bg-1">
               <div class="carousel-container">
                 <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
                 <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
@@ -109,7 +109,7 @@
             </div>
     
             <!-- Slide 3 -->
-            <div class="carousel-item">
+            <div class="carousel-item slide-bg-1">
               <div class="carousel-container">
                 <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
                 <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
